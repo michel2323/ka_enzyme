19d28329f3f18f176d3d55c05972c39d412f88f8
Michel Schanen <mschanen@anl.gov>
2024-02-15 11:15:01 -0600

    CPU crash

--- mwe.jl
+++ mwe.jl
@@ -2,11 +2,12 @@ using Test
 using Enzyme
 using KernelAbstractions
 using CUDA
-
+const KA = KernelAbstractions
 @kernel function id!(A,B)
     I = @index(Global, Linear)
     @inbounds A[I] = B[I]
     @synchronize()
+    println("hello")
     return nothing
 end
 
@@ -17,24 +18,25 @@ function id_caller(A, B, backend)
 end
 
 function enzyme_testsuite(backend, ArrayT)
-    @testset "kernels" begin
-        A = CuArray(zeros(64))
-        dA = ArrayT{Float64}(undef, 64)
-        B = ArrayT{Float64}(undef, 64)
-        dB = ArrayT{Float64}(undef, 64)
+    A = ArrayT(zeros(64))
+    dA = ArrayT{Float64}(undef, 64)
+    B = ArrayT{Float64}(undef, 64)
+    dB = ArrayT{Float64}(undef, 64)
 
-        dA .= 1
-        B .= (1:1:64)
-        dB .= 1
-
-        Enzyme.autodiff(ReverseWithPrimal, id_caller, Duplicated(A, dA), Duplicated(B, dB), Const(backend()))
-        KernelAbstractions.synchronize(backend())
-        @show A, B
-        @show dA, dB
-    end
+    dA .= 1
+    B .= (1:1:64)
+    dB .= 1
+    # id_caller(A, B, backend())
+    KernelAbstractions.synchronize(backend())
+    Enzyme.autodiff(ReverseWithPrimal, id_caller, Duplicated(A, dA), Duplicated(B, dB), Const(backend()))
+    KernelAbstractions.synchronize(backend())
+    @show A
+    @show B
+    @show dA
+    @show dB
 end
 
-@assert CUDA.functional()
-@assert CUDA.has_cuda_gpu()
+# @assert CUDA.functional()
+# @assert CUDA.has_cuda_gpu()
 # enzyme_testsuite(CPU, Array)
-enzyme_testsuite(CUDABackend, CuArray)
+enzyme_testsuite(CPU, Array)
