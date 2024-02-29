4d69b3bd9ce93115d35594ffa88a6f7eb4d33e8d
Michel Schanen <mschanen@anl.gov>
2024-02-29 11:16:55 -0600

    Working reverse CUDA

--- mwe.jl
+++ mwe.jl
@@ -2,17 +2,18 @@ using Test
 using Enzyme
 using KernelAbstractions
 using CUDA
+
 const KA = KernelAbstractions
-@kernel function id!(A,B)
+
+@kernel function square!(A,B)
     I = @index(Global, Linear)
-    @inbounds A[I] = B[I]
+    @inbounds A[I] = B[I] * B[I]
     @synchronize()
-    println("hello")
     return nothing
 end
 
-function id_caller(A, B, backend)
-    kernel = id!(backend)
+function square_caller(A, B, backend)
+    kernel = square!(backend)
     kernel(A, B, ndrange=size(A))
     return nothing
 end
@@ -26,17 +27,16 @@ function enzyme_testsuite(backend, ArrayT)
     dA .= 1
     B .= (1:1:64)
     dB .= 1
-    # id_caller(A, B, backend())
-    KernelAbstractions.synchronize(backend())
-    Enzyme.autodiff(ReverseWithPrimal, id_caller, Duplicated(A, dA), Duplicated(B, dB), Const(backend()))
-    KernelAbstractions.synchronize(backend())
+    KA.synchronize(backend())
+    Enzyme.autodiff(ReverseWithPrimal, square_caller, Duplicated(A, dA), Duplicated(B, dB), Const(backend()))
+    KA.synchronize(backend())
     @show A
     @show B
     @show dA
     @show dB
 end
 
-# @assert CUDA.functional()
-# @assert CUDA.has_cuda_gpu()
-# enzyme_testsuite(CPU, Array)
 enzyme_testsuite(CPU, Array)
+if CUDA.functional() && CUDA.has_cuda_gpu()
+    enzyme_testsuite(CUDABackend, CuArray)
+end
