4e2ff68c69072066a4ef1882c63d5fc5e4a4ebb4
Michel Schanen <mschanen@anl.gov>
2024-04-01 15:40:06 -0500

    MWE update

--- mwe.jl
+++ mwe.jl
@@ -2,19 +2,38 @@ using Test
 using Enzyme
 using KernelAbstractions
 using CUDA
+using EnzymeCore
+using EnzymeCore.EnzymeRules
 
 const KA = KernelAbstractions
-
-@kernel function square!(A,B)
-    I = @index(Global, Linear)
-    @inbounds A[I] = B[I] * B[I]
-    @synchronize()
-    return nothing
+function EnzymeRules.augmented_primal(
+    config::Config,
+    func::Const{typeof(KA.synchronize)},
+    ::Type{Const{Nothing}},
+    backend::T
+) where T <: EnzymeCore.Annotation
+    println("Forward synchronize for $(typeof(backend))")
+    return AugmentedReturn{Nothing, Nothing, Any}(
+        nothing, nothing, (nothing)
+    )
 end
 
+function EnzymeRules.reverse(config::Config, func::Const{typeof(KA.synchronize)}, ::Type{Const{Nothing}}, tape, backend)
+    println("Reverse synchronize for $(typeof(backend))")
+    return (nothing,)
+end
+const KA = KernelAbstractions
+
+# @kernel function square!(A,B)
+#     I = @index(Global, Linear)
+#     @inbounds A[I] = B[I] * B[I]
+#     @synchronize()
+# end
+
 function square_caller(A, B, backend)
-    kernel = square!(backend)
-    kernel(A, B, ndrange=size(A))
+    # kernel = square!(backend)
+    # kernel(A, B, ndrange=size(A))
+    KA.synchronize(backend)
     return nothing
 end
 
@@ -27,16 +46,20 @@ function enzyme_testsuite(backend, ArrayT)
     dA .= 1
     B .= (1:1:64)
     dB .= 1
+    x = [2.0]
+    dx = [0.0]
+    # Enzyme.autodiff(
+    #     ReverseWithPrimal, square_caller, Duplicated(A, dA),
+    #     Duplicated(B, dB), Duplicated(x,dx)
+    # )
+    Enzyme.autodiff(
+        ReverseWithPrimal, square_caller, Duplicated(A, dA),
+        Duplicated(B, dB), Const(backend())
+    )
     KA.synchronize(backend())
-    Enzyme.autodiff(ReverseWithPrimal, square_caller, Duplicated(A, dA), Duplicated(B, dB), Const(backend()))
-    KA.synchronize(backend())
-    @show A
-    @show B
-    @show dA
-    @show dB
 end
 
+# Doesn't trigger rules above
 enzyme_testsuite(CPU, Array)
-if CUDA.functional() && CUDA.has_cuda_gpu()
-    enzyme_testsuite(CUDABackend, CuArray)
-end
+# Triggers rules above
+enzyme_testsuite(CUDABackend, CuArray)
